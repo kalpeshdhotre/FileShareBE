1df9eacc68dcdb3e74f3930cb98544c3bf65fcc7
Kalpesh KD <kalpeshdhotre@hotmail.com>
2025-05-06 16:53:07 +0530

    uploadFiles function fixed for thumbnmail

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,45 +1,71 @@
-import { File } from '../models/file.models.js';
+import { File } from "../models/file.models.js";
 
+// const uploadFiles = async (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).json({ error: 'No files uploaded' });
+//   }
 
+//   const fileObj={
+//     path: req.file.path,
+//     name: req.file.originalname,
+//     type: req.file.mimetype,
+//     size: req.file.size
+//   }
+//   const SERVER_URL=process.env.SERVER_URL;
+
+//   try {
+//     const file=await File.create(fileObj);
+//     res.status(200).json({
+//         msg:"File Uploaded Successfully",
+//         path:`${SERVER_URL}/file/${file._id}`,
+//         data:file
+//     })
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+
+// };
+// ... existing code ...
 
 const uploadFiles = async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No files uploaded' });
-  }
- 
-  const fileObj={
-    path: req.file.path,
-    name: req.file.originalname, 
-    type: req.file.mimetype, 
-    size: req.file.size
-  }
-  const SERVER_URL=process.env.SERVER_URL;
+	if (!req.file) {
+		return res.status(400).json({ error: "No files uploaded" });
+	}
 
-  try {
-    const file=await File.create(fileObj);
-    res.status(200).json({
-        msg:"File Uploaded Successfully",
-        path:`${SERVER_URL}/file/${file._id}`,
-        data:file
-    })
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-  
+	const SERVER_URL = process.env.SERVER_URL;
+
+	// Use the filename for the public URL
+	const fileObj = {
+		path: `${SERVER_URL}/uploads/${req.file.filename}`,
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size,
+	};
+
+	try {
+		const file = await File.create(fileObj);
+		res.status(200).json({
+			msg: "File Uploaded Successfully",
+			path: fileObj.path, // <-- This is the public URL for preview
+			data: file,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
-const downloadFile=async (req,res)=>{
-    try {
-        const file=await File.findById(req.params.fileId);
-        file.downloadedContent++;
-        await file.save();
-        res.download(file.path,file.name);
-    } catch (error) {
-        console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
+const downloadFile = async (req, res) => {
+	try {
+		const file = await File.findById(req.params.fileId);
+		file.downloadedContent++;
+		await file.save();
+		res.download(file.path, file.name);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
 
-
-export {uploadFiles,downloadFile}
+export { uploadFiles, downloadFile };
